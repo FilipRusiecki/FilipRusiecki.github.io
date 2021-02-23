37b6b291e981d54ea9c7d1cc3ae283cfe76553c0
FilipRusiecki11 <fil201222@gmail.com>
2021-02-23 10:51:55 +0000

    WPA test

---
+++ service_worker.js
@@ -0,0 +1,25 @@
+var cacheName = 'muddygames-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/push.css',
+  '/js/script.js'
+];
+
+/* Cache contents when Offline See Cache */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline, examine Cache Storage */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
